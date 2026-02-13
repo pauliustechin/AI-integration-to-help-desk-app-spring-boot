ec05697a073848bf2665d3349100c84442b873e2
pauliustechin <pauliustechin@gmail.com>
2026-02-13 20:24:46 +0200

    setup project env, create ChatController

---
+++ src/test/java/com/psem/Spring/boot/with/Ollama/SpringBootWithOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psem.Spring.boot.with.Ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
